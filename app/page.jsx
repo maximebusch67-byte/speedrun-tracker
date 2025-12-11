b980edccd060550a96b6b05e43b1868533fd845b
maximebusch67-byte <maxime.busch67@gmail.com>
2025-12-11 02:28:23 +0100

    Create page.jsx

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>Subway Leaderboard</h1>
+      <p>Bienvenue bg.</p>
+
+      <a href="/login" style={{ fontSize: 20 }}>➡ Login Discord</a>
+      <br /><br />
+      <a href="/leaderboard" style={{ fontSize: 20 }}>➡ Voir le classement</a>
+    </div>
+  );
+}
+²
